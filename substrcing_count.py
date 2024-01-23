a1ee0a040fcbbec73824c28be9f51edffefd74c9
MJ Gillado <mjgillado0@gmail.com>
2024-01-23 18:03:55 +0800

    Created pseudocode

--- substrcing_count.py
+++ substrcing_count.py
@@ -1,2 +1,8 @@
-# Write a program to find how many times substring “Emma” appears in the given string.
+# Write a program to find how many times substring 
+#“Emma” appears in the given string.
 
+# Pseudocode
+
+# Paste the given 
+# Use string count method to determine the "Emma" name count appearance
+# Print how many times name "Emma" appears
