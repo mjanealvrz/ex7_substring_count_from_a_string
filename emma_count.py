acf06d92dcef29d21e486d1b6159cd8861e7a265
MJ Gillado <mjgillado0@gmail.com>
2024-01-23 18:13:47 +0800

    Created string count method and print result

--- emma_count.py
+++ emma_count.py
@@ -8,5 +8,6 @@ import string
 # Paste the given 
 example_string = "Emma is good developer. Emma is a writer"
 # Use string count method to determine the "Emma" name count appearance
-
+emma_count = example_string.count("Emma")     
 # Print how many times name "Emma" appears
+print("Emma's name appeared", emma_count, "times.")
