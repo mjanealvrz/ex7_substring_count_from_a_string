952a4153aa6a6c6cd3d3f68b01e43fda817f57f5
MJ Gillado <mjgillado0@gmail.com>
2024-01-23 18:09:50 +0800

    Imported string

--- substrcing_count.py
+++ substrcing_count.py
@@ -1,8 +1,12 @@
 # Write a program to find how many times substring 
 #“Emma” appears in the given string.
 
+import string
+
 # Pseudocode
 
 # Paste the given 
+example_string = "Emma is good developer. Emma is a writer"
 # Use string count method to determine the "Emma" name count appearance
+
 # Print how many times name "Emma" appears
